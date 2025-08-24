95bc66dc64754aecdcde1e8dfc061e37a176f0c7
Chris <ChristoWindow@gmail.com>
2025-08-24 00:52:42 -0400

    Update Home.tsx

--- Frontend/src/screens/Home.tsx
+++ Frontend/src/screens/Home.tsx
@@ -3,9 +3,15 @@ import React from 'react';
 const Home : React.FC = () => {
     return (
         <div className="h-screen flex items-center justify-center  bg-[#c7c4ff]">
-            <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-[length:300%_100%] bg-gradient-to-r from-maya-blue via-white via-pink-200 to-amouranth-pink animate-gradient-x inline-block">               Niora
-            </h1>
+            <div className="h-screen flex items-center justify-center  bg-[#c7c4ff]">
+                <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-[length:300%_100%] bg-gradient-to-r from-maya-blue via-white via-pink-200 to-amouranth-pink animate-gradient-x inline-block"> Niora
+                </h1>
+            </div>
+
+            <div>
+
+            </div>
         </div>
-    );
-};
-export default Home;
+            );
+            };
+            export default Home;
